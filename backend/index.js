95deca9e903d11ccf8d097790e2aeef2f2af76e0
vipbit10 <143544793+vipbit10@users.noreply.github.com>
2025-05-16 16:45:17 +0530

    vip fixed again

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://vip-jobportal.onrender.com',
     credentials:true
 }
 
